aaf4af8570dad891896c707ff0466585d456e7e3
Harsh <harshpardhi477@gmail.com>
2026-05-26 09:51:44 +0530

    Day 6 orphan Process

---
+++ temp_cleaner.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import tempfile
+
+temp_dir = tempfile.gettempdir()
+
+for item in os.listdir(temp_dir):
+    path = os.path.join(temp_dir, item)
+
+    try:
+        if os.path.isfile(path) or os.path.islink(path):
+            os.unlink(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path, ignore_errors=True)
+    except:
+        pass
+
+print("Temp files cleaned successfully.")
